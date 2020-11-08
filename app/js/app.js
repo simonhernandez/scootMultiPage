9ccc5464dd7f626bba41e84393f7a25275ea1b53
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-11-07 23:01:13 -0400

    add toggle icon function

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,24 @@
 // Variables
 const navToggle = document.getElementById('navToggle');
 const navMenu = document.getElementById('menu');
+let toggle = false;
 
 // Functions
 
+function toggleIcon(){
+
+    if (toggle){
+        navToggle.style.backgroundImage = 'url(../../assets/icons/hamburger.svg)';
+    } else {
+        navToggle.style.backgroundImage = 'url(../../assets/icons/close.svg)';
+    }
+
+    toggle = !toggle;
+}
+
 // Main
 navToggle.addEventListener('click', () => {
-    alert('test');
+    navMenu.classList.toggle('showMenu');
+
+    toggleIcon();
 });
