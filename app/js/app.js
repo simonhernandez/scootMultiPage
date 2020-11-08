4f0e490af68fd8417387868d23bd3e82459bb919
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2020-11-07 22:50:19 -0400

    test script

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+// Variables
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('menu');
+
+// Functions
+
+// Main
+navToggle.addEventListener('click', () => {
+    alert('test');
+});
